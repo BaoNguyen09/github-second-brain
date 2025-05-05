37cd53c48e060ea57eb703356aa41a5afa6d2e91
BaoNguyen09 <nttbvlogs@gmail.com>
2025-05-05 07:49:23 -0700

    feat: add more extensive error handling and messages

--- main.py
+++ main.py
@@ -15,11 +15,28 @@ async def root():
 @app.post("/api/v1/process", status_code=200)
 async def process_repo(repo_req: RepoRequest, response: Response):
     # This method will return 200 if the repo is already processed
-    # Else it will process and return 201
+    # If it wasn't processed, it will be ingested and return 201
+    # Else it will return some error messages
 
+    repo_url_str = str(repo_req.repo_url)
     # Get all processed repo and check if incoming repo was processed
-    if ingest_repo(str(repo_req.repo_url)):
-        response.status_code = status.HTTP_201_CREATED
-        return {"message": "Repo has been ingested successfully"}
+    try:
+        res = ingest_repo(repo_url_str)
+        if res[0]: # Should return True only on successful *new* processing
+            response.status_code = status.HTTP_201_CREATED
+            return {"message": f"Repository {repo_url_str} ingested successfully."}
+        
+        else:
+            # Handle different error cases
+            if res[1] == "Repository was already processed previously.":
+                return {"message": res[1]}
+             
+            response.status_code = status.HTTP_400_BAD_REQUEST
+            return {"message": res[1]}
+        
+    except Exception as e:
+         print(f"ERROR in API processing {repo_url_str}: {e}") # Log the error
+         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+         return {"message": f"Failed to process repository {repo_url_str} due to an internal error."}
     
-    return {"message": "This repo was processed"}
+    
