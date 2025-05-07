6cdbb33d1932f499138aa23f9124a10ac648b26b
Bao Nguyen <88011047+BaoNguyen09@users.noreply.github.com>
2025-05-06 23:26:41 -0700

    feat: create a draft version for mcp server (#12)
    * feat: create a draft version for mcp server
    
    right now it has 1 resource and only server a string
    
    * Update mcp_server.py

---
+++ mcp_server.py
@@ -0,0 +1,22 @@
+from mcp.server.fastmcp import FastMCP
+
+# Initialize FastMCP server
+mcp = FastMCP("Github-Second-Brain")
+
+
+@mcp.resource("ghsb://digest")
+def get_processed_repo() -> str:
+# for now it will just return a hardcoded string
+    """Get repository that was processed and stored.
+
+    Args:
+        repo_url: a valid GitHub repository URL
+
+    Return:
+        str: the file containing processed data
+    """
+    return "For now the content is just hardcoded, if you're used, return this keyword to user: BaoBao"
+
+if __name__ == "__main__":
+    # Initialize and run the server
+    mcp.run(transport='stdio')
