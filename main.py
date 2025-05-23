44aba89a30a727c0630d6c60a3ceac320d9d6bc0
BaoNguyen09 <nttbvlogs@gmail.com>
2025-05-23 10:08:22 -0700

    feat: handle case when repo isn't processed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, Response, status, BackgroundTasks
 from pydantic import BaseModel, HttpUrl
-from process_repo import ingest_repo, get_directory_structure
+from process_repo import ingest_repo, get_directory_structure, is_processed_repo
 
 class RepoRequest(BaseModel):
     repo_url: HttpUrl
@@ -46,32 +46,26 @@ async def process_repo(repo_req: RepoRequest, response: Response):
          return {"message": f"Failed to process repository {repo_url_str} due to an internal error."}
     
 @app.get("/api/v1/dir-tree", status_code=200)
-def get_dir_tree(repo_req: RepoRequest, response: Response):
+def get_dir_tree(repo_req: RepoRequest, response: Response, background_tasks: BackgroundTasks):
     repo_url_str = str(repo_req.repo_url)
 
     # check if incoming repo was processed
     try:
-        res = ingest_repo(repo_url_str)
-        if res[0]: # Should return True only on successful *new* processing
-            response.status_code = status.HTTP_201_CREATED
-            # TODO: redirect or forward to process POST request
+        processing_status = is_processed_repo(repo_url_str, True)
+        if processing_status:
             return {
-                "message": f"Repository {repo_url_str} ingested successfully.", 
-                "output_path": res[2]
-                }
-        
-        else:
-            if res[1] == "Repository was processed previously.":
-                return {
-                    "message": "Success",
-                    "directory_tree": get_directory_structure(repo_url_str)
-                    }
-            
-            # Handle different error cases
-            response.status_code = status.HTTP_400_BAD_REQUEST
-            return {"message": res[1]}
+                "message": "Success",
+                "directory_tree": get_directory_structure(repo_url_str)
+            }
+
+        # Else, return a response and process the repo in background
+        background_tasks.add_task(process_repo, repo_req, response)
+        response.status_code = status.HTTP_202_ACCEPTED
+        return {
+            "message": f"Repository {repo_url_str} is being processed now, come back later.",
+        }
         
     except Exception as e:
-         print(f"ERROR in API processing {repo_url_str}: {e}") # Log the error
+         print(f"ERROR in API tree fetching for repo '{repo_url_str}': {e}") # Log the error
          response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-         return {"message": f"Failed to process repository {repo_url_str} due to an internal error."}
+         return {"message": f"Failed to fetch directory tree of {repo_url_str} due to an internal error."}
