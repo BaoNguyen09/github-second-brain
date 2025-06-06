d9815858fd36ad9da355a42fea72d9d800a983cc
BaoNguyen09 <nttbvlogs@gmail.com>
2025-06-06 11:36:33 -0700

    refactor: remove deprecated/unused endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import Depends, FastAPI, HTTPException, Path, Response, status, BackgroundTasks
 from pydantic import BaseModel, HttpUrl
 from app.internal_tools.github_api_client import fetch_issue_context, GitHubApiError, fetch_directory_tree_with_depth, fetch_file_contents
-from app.internal_tools.process_repo import ingest_repo, get_directory_structure, is_processed_repo, get_a_file_content
 from typing import List, Optional
 import httpx
 
@@ -39,40 +38,7 @@ async def get_http_client():
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
-
-@app.post("/api/v1/process", status_code=200)
-async def process_repo(repo_req: RepoRequest, response: Response):
-    # This method will return 200 if the repo is already processed
-    # If it wasn't processed, it will be ingested and return 201
-    # Else it will return some error messages
-
-    repo_url_str = str(repo_req.repo_url)
-    # Get all processed repo and check if incoming repo was processed
-    try:
-        res = ingest_repo(repo_url_str)
-        if res[0]: # Should return True only on successful *new* processing
-            response.status_code = status.HTTP_201_CREATED
-            return {
-                "message": f"Repository {repo_url_str} ingested successfully.", 
-                "output_path": res[2]
-                }
-        
-        else:
-            if res[1] == "Repository was processed previously.":
-                return {
-                    "message": res[1],
-                    "output_path": res[2]
-                    }
-            
-            # Handle different error cases
-            response.status_code = status.HTTP_400_BAD_REQUEST
-            return {"message": res[1]}
-        
-    except Exception as e:
-         print(f"ERROR in API processing {repo_url_str}: {e}") # Log the error
-         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-         return {"message": f"Failed to process repository {repo_url_str} due to an internal error."}
+    return {"message": "Welcome to GitHub Second Brain!"}
 
 @app.get("/api/v1/directory-tree/{owner}/{repo}", status_code=200)
 async def get_tree(
